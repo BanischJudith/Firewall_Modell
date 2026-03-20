3f40290e9e93f5930ba9dae1d8a9299c00b0c758
Judith Banisch <jutka03@icloud.com>
2026-03-20 13:44:05 +0100

    Funktion 'eingabe' in Funktion 'main' eingebettet, Print-Sätze umgeschrieben

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 
 def eingabe():
     ip = input("Gib eine IP-Adrsse ein: ")
-    print("Die Firewall hat " + ip + " abgelehnt")
+    return ip
 
 def main():
     erlaubte_ips = {
         "111.111.1.1": "erlaubt"
     }
 
-    if "111.111.1.1" in erlaubte_ips:
-        print(erlaubte_ips.get('111.111.1.1'))
+    ip = eingabe()
+
+    if ip in erlaubte_ips:
+        print("Die Firewall von " + ip + " hat die Verbindung erlaubt")
     else:
-        print("verweigert")
-        
+        print("Die Firewall von " + ip + " hat die Verbindung geblockt")
+
 eingabe()
 main()
