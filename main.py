d208f5159c34b152e71791ab0a3fe3c242ae7f37
Judith Banisch <jutka03@icloud.com>
2026-03-27 13:33:31 +0100

    Variable 'status' erstellt, 'status' sieht nach ob 'ip' in 'erlaubte_ips' ist, If-Statement mit 'status' verknüpft, Tippfehler korrigiert, Strings zu F-Strings formatiert

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """docstring"""
 
 def eingabe():
-    ip = input("Gib eine IP-Adrsse ein: ")
+    ip = input("Gib eine IP-Adresse ein: ")
     return ip
 
 def main():
@@ -11,9 +11,11 @@ def main():
 
     ip = eingabe()
 
-    if ip in erlaubte_ips:
-        print("Die Firewall von " + ip + " hat die Verbindung erlaubt")
+    status = erlaubte_ips.get(ip)
+
+    if status:
+        print(f"Die Firewall von {ip} hat die Verbindung erlaubt")
     else:
-        print("Die Firewall von " + ip + " hat die Verbindung geblockt")
+        print(f"Die Firewall von {ip} hat die Verbindung geblockt")
 
 main()
