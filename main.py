4f863d3fb7565ea6c63c5d1f0077c450c2620a7c
Judith Banisch <jutka03@icloud.com>
2026-03-31 11:13:47 +0200

    Modul ipaddress importiert, Funktion 'str_zu_int' erstellt, 'ip' umbenannt in 'eing_ip', 'erlaubte_ips' umbenannt in 'ip_dict', 'status' entfernt, ip_dict: Schlüssel zu Tuple mit str_zu_int geändert, For_Loop für ip_dict Tuples erstellt, Variable 'ip' erstellt, Docstrings hinzugefügt

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-"""docstring"""
+import ipaddress
+
+def str_zu_int(str_ip):
+    """Verwandelt Strings in Integers im IPv4-Format."""
+    return int(ipaddress.ip_address(str_ip))
 
 def eingabe():
-    ip = input("Gib eine IP-Adresse ein: ")
-    return ip
+    """Der User gibt die zu testende IP ein. Der eingegebene Input hat die Variable eing_ip."""
+    eing_ip = input("Gib eine IP-Adresse ein: ")
+    return eing_ip
 
 def main():
-    erlaubte_ips = {
-        "111.111.1.1": "erlaubt"
+    """Dictionary mit Integer-Zahlenumfängen im IPv4-Format. 
+       User wird gefragt, eine IP einzugeben (Variable eing_ip).
+       Eing_ip wird zu Integer (Variable ip).
+       For-Loop: Schaut, ob ip im Umfang im Dictionary ist."""
+    ip_dict = {
+        (str_zu_int("192.168.1.0"), str_zu_int("192.168.1.255")): "erlaubt"
     }
 
-    ip = eingabe()
+    eing_ip = eingabe()
 
-    status = erlaubte_ips.get(ip)
+    ip = str_zu_int(eing_ip)
 
-    if status:
-        print(f"Die Firewall von {ip} hat die Verbindung erlaubt")
-    else:
-        print(f"Die Firewall von {ip} hat die Verbindung geblockt")
+    for (start, ende), wert in ip_dict.items():
+        if start <= ip <= ende:
+            print(f"Die Firewall von {eing_ip} hat die Verbindung erlaubt")
+        else:
+            print(f"Die Firewall von {eing_ip} hat die Verbindung geblockt")
 
 main()
