39b5ed085c93d288f547fe0cc5f78e6d59916e22
Judith Banisch <jutka03@icloud.com>
2026-04-01 16:33:13 +0200

    Funk 'eingabe': Var 'eing_port' hinzugefügt, 'ip_dict': nested Dict 'port_dict' angefangen, Funk 'main': 'eingabe' ergänzt mit 'eing_port', For-Loop: If-Else-Statement hinzugefügt das 'port_dict' kontrolliert

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def str_zu_int(str_ip):
 def eingabe():
     """Der User gibt die zu testende IP ein. Der eingegebene Input hat die Variable eing_ip."""
     eing_ip = input("Gib deine IP-Adresse ein: ")
-    return eing_ip
+    eing_port = input("Gib den Port ein, auf den du Zugriff möchtest: ")
+    return eing_ip, eing_port
 
 def main():
     """Dictionary mit Integer-Zahlenumfängen im IPv4-Format. 
@@ -15,7 +16,9 @@ def main():
        Eing_ip wird zu Integer (Variable ip).
        For-Loop: Schaut, ob ip im Umfang im Dictionary ist."""
     ip_dict = {
-        (str_zu_int("1.0.0.0"), str_zu_int("9.255.255.255")): "placeholder",
+        (str_zu_int("1.0.0.0"), str_zu_int("9.255.255.255")): {
+          "80": "test-port"  
+        },
         (str_zu_int("11.0.0.0"), str_zu_int("126.255.255.255")): "placeholder",
         (str_zu_int("129.0.0.0"), str_zu_int("169.253.255.255")): "placeholder",
         (str_zu_int("169.255.0.0"), str_zu_int("172.15.255.255")): "placeholder",
@@ -23,16 +26,20 @@ def main():
         (str_zu_int("192.0.3.0"), str_zu_int("192.88.98.255")): "placeholder",
         (str_zu_int("192.88.100.0"), str_zu_int("192.167.255.255")): "placeholder",
         (str_zu_int("192.169.0.0"), str_zu_int("198.17.255.255")): "placeholder",
-        (str_zu_int("198.20.0.0"), str_zu_int("223.255.255.255")): "placeholder"        
+        (str_zu_int("198.20.0.0"), str_zu_int("223.255.255.255")): "placeholder"
     }
 
-    eing_ip = eingabe()
+    eing_ip, eing_port = eingabe()
 
     ip = str_zu_int(eing_ip)
 
-    for (start, ende), wert in ip_dict.items():
+    for (start, ende), port_dict in ip_dict.items():
         if start <= ip <= ende:
             print(f"Die Firewall hat die Verbindung mit deiner IP-Adresse {eing_ip} zugelassen.")
+            if eing_port in port_dict:
+                print(f"Port {eing_port} ist offen.")
+            else: 
+                print(f"Port {eing_port} ist geschlossen.")
             break
     else:
         print(f"Die Firewall hat die Verbindung mit deiner IP-Adresse {eing_ip} verhindert.")
