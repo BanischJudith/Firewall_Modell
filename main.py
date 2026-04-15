af9c15b1c0f0ac45f3f81a8ad605d2b9b959cc76
Judith Banisch <jutka03@icloud.com>
2026-04-15 11:36:35 +0200

    Docstrings angepasst

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ def str_zu_int(str_ip):
     return int(ipaddress.ip_address(str_ip))
 
 def eingabe():
-    """Der User gibt die zu testende IP ein. Der eingegebene Input hat die Variable eing_ip."""
+    """Der User gibt die zu testende IP ein. Der eingegebene Input hat die Variable eing_ip und eing_port."""
     eing_ip = input("Gib deine IP-Adresse ein: ")
     eing_port = input("Gib den Port ein, auf den du Zugriff möchtest: ")
     return eing_ip, eing_port
 
 def main():
-    """Dictionary mit Integer-Zahlenumfängen im IPv4-Format. 
-       User wird gefragt, eine IP einzugeben (Variable eing_ip).
-       Eing_ip wird zu Integer (Variable ip).
-       For-Loop: Schaut, ob ip im Umfang im Dictionary ist."""
+    """(1) Dictionary mit Integer-Zahlenumfängen im IPv4-Format. 
+       (2) User wird gefragt, eine IP und einen Port einzugeben (Variablen eing_ip und eing_port).
+       (3) Eing_ip wird zu Integer (Variable ip).
+       (4) For-Loop: Schaut, ob ip im Umfang im Dictionary ist."""
     ip_dict = {
         (str_zu_int("1.0.0.0"), str_zu_int("9.255.255.255")): {
           "80": "test-port"  
@@ -38,7 +38,7 @@ def main():
             print(f"Die Firewall hat die Verbindung mit deiner IP-Adresse {eing_ip} zugelassen.")
             if eing_port in port_dict:
                 print(f"Port {eing_port} ist offen.")
-            else: 
+            else:
                 print(f"Port {eing_port} ist geschlossen.")
             break
     else:
